fb4f02a44eaf5b04d16336e5cea0097b54a6d4cb
Vetter <kasper.vetter@mail.dk>
2017-12-18 15:52:53 +0100

    changed back to  use discordgo

--- main.go
+++ main.go
@@ -1,18 +1,81 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
 )
 
 func main() {
+
+	discord, err := discordgo.New("MzU1Nzg1MTI3MjE5MDM2MTYy.DRhloQ.XlWSs42qIAQj2pUG35Q4BIZhXBk")
+	// if there is an error, we print it to the console
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	discord.AddHandler(messageCreate)
+	// Open a websocket connection to Discord and begin listening.
+	err = discord.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	discord.Close()
+
+}
+
+// This function will be called (due to AddHandler above) every time a new
+// message is created on any channel that the autenticated bot has access to.
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	// If the message is "ping" reply with "Pong!"
+	if m.Content == "ping" {
+		s.ChannelMessageSend(m.ChannelID, "Pong!")
+	}
+
+	// If the message is "pong" reply with "Ping!"
+	if m.Content == "pong" {
+		s.ChannelMessageSend(m.ChannelID, "Ping!")
+	}
+}
+
+// tried to make my own http connection to discord, but seems more complex than first anticipated
+func request() {
+
 	fmt.Println("Start api call")
-	b := make([]byte, 20)
-	req, err := http.NewRequest("GET", "https://discordapp.com/api/v6", bytes.NewBuffer(b))
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", "https://discordapp.com/api/v6", nil)
+	// ...
+	req.Header.Set("Authorization", "Bot MzU1Nzg1MTI3MjE5MDM2MTYy.DRhloQ.XlWSs42qIAQj2pUG35Q4BIZhXBk")
+
+	req.Header.Set("Content-Type", "application/json")
+	// TODO: Make a configurable static variable.
+	req.Header.Set("User-Agent", "https://github.com/Kvetter/discord-go-bot")
+	resp, err := client.Do(req)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println(*req)
+	fmt.Println(*resp)
 }
